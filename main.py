512164d3024f2ab0ce6da193f42ed2c17b8ce7cd
Lucas Alves <lucasalves_taz@hotmail.com>
2025-03-05 11:27:08 -0300

    up main.py

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from typing import Union
-from fastapi import FastAPI
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 
-app = FastAPI()
+import auth
+import crud
+import schemas
+from database import SessionLocal
+
+router = APIRouter(prefix="/users", tags=["users"])
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@router.post("/{user_id}", response_model=schemas.UserResponse)
+def read_user(
+    user_id: int,
+    db: Session = Depends(get_db),
+    current_user: schemas.UserResponse = Depends(auth.get_current_user),
+):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
