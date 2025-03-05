43d6ba6b11d04167708db91dafb0583ebbb30163
Lucas Alves <lucasalves_taz@hotmail.com>
2025-03-05 12:50:00 -0300

    up f routes

--- routes/auth.py
+++ routes/auth.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-import auth, schemas
+
+import auth
+import schemas
 from database import SessionLocal
 
 router = APIRouter(prefix="/auth", tags=["auth"])
+
 
 def get_db():
     db = SessionLocal()
@@ -15,17 +18,19 @@ def get_db():
 
 
 @router.post("/login")
-def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
     user = auth.authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
         )
     access_token = auth.create_acess_token(data={"sub": user.email})
     return {"access_token": access_token, "token_type": "bearer"}
 
+
 @router.get("/me", response_model=schemas.UserResponse)
-def read_users_me(current_user: schemas.User = Depends(auth.get_current_user)):
+def read_users_me(current_user: schemas.UserResponse = Depends(auth.get_current_user)):
     return current_user
